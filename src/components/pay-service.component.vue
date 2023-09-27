cb1906f38a418eb918b50314dd25dcf3bcbb3590
OnofreR <carlosonofreruiz16@gmail.com>
2023-09-27 10:23:19 -0500

    first version all complete

--- src/components/pay-service.component.vue
+++ src/components/pay-service.component.vue
@@ -1,45 +1,48 @@
 <script setup>
 import { ref } from 'vue';
+import DataFavoritesComponent from "@/components/services-data-favorites.component.vue";
+import GalleriaComponent from "@/components/services-galleria.component.vue";
+import SuccessfulPaymentComponent from "@/components/successful-payment.component.vue";
 
 const value = ref('');
 const value2 = ref(1);
-const value3 = ref('');
-const visible = ref(false);
+const value3 = ref(null);
 const value4 = ref(null);
-
+const visible = ref(false);
 </script>
 
 <template>
   <div class="save">
+
     <pv-button class="btn" label="Pagar Oferta"  @click="visible = true" />
-    <pv-dialog v-model:visible="visible" modal header="Pagar Servicio" :style="{ width: '40rem' }"
+
+
+    <pv-dialog v-model:visible="visible" modal header="Pagar Servicio" :style="{ width: '50rem' }"
                :breakpoints="{ '960px': '75vw', '641px': '100vw' }">
 
       <h1> Servicio Seleccionado </h1>
 
-      <pv-card class="tourist-profile" style="width: 35rem">
+      <pv-card class="tourist-profile" style="width: 45rem">
 
         <template #content>
-          <img src="https://primefaces.org/cdn/primevue/images/galleria/galleria1.jpg" alt="servicio 1" style="width: 15rem">
-          <h1 > Alpinismo para principiantes en el Huascarán, se parte de la aventura de vivir  </h1>
-          <ul>
-            <li><b>Por:</b><p>agencia TravelNew</p></li>
-            <b>Fecha de Salida: </b><p>12/05/2023</p>
-            <b>Total de Importe: </b><p></p>
-            <div class="flex-auto">
-              <b>Total de personas: </b> <pv-inputNumber class="input-number" v-model="value2" inputId="minmax-buttons" mode="decimal" showButtons :min="0" :max="100" />
-            </div>
-          </ul>
-
-
-
+          <div class="content flex gap-5 justify-content-between align-items-center">
+            <galleria-component></galleria-component>
+            <h1 > Alpinismo para principiantes en el Huascarán, se parte de la aventura de vivir  </h1>
+            <ul>
+              <li><b>Por:</b><p>agencia TravelNew</p></li>
+              <li><b>Fecha de Salida: </b><p>12/05/2023</p></li>
+              <li><b>Total de Importe: </b><p>$500.00</p></li>
+              <div class="flex-auto">
+                <b>Total de personas: </b> <pv-inputNumber class="input-number" v-model="value2" inputId="minmax-buttons" mode="decimal" showButtons :min="0" :max="100" />
+              </div>
+            </ul>
+          </div>
         </template>
       </pv-card>
-
-      <h1> Métodos de Pago </h1>
+      <br><h1> Métodos de Pago </h1><br>
       <div class="card flex flex-wrap justify-content-left gap-3">
         <span class="p-input-icon-left">
-            <pv-inputText class= "pay" v-model="value4" placeholder="Numero de tarjeta debito/credito" />
+            <pv-inputText class= "pay" v-model="value3" placeholder="Numero de tarjeta debito/credito" />
             </span>
       </div>
 
@@ -48,8 +51,7 @@ const value4 = ref(null);
             <pv-inputText class= "pay" v-model="value4" placeholder="Nombre del titular de la cuenta" />
             </span>
       </div>
-
-      <pv-button class="btn-calculate btn" label="Aplicar" @click="visible = false" />
+      <successful-payment-component></successful-payment-component>
     </pv-dialog>
   </div>
 </template>
@@ -61,10 +63,10 @@ const value4 = ref(null);
   font-size: 0.8em;
 }
 .input-number {
-  width: 40%;
+  width: 80%;
 }
 .pay {
-  width: 200%;
+  width: 290%;
 }
 .discount {
   font-size: 0.8em;
@@ -79,6 +81,10 @@ ul {
 }
 ul li {
   font-size: 0.9em;
+}
+.galleria {
+  overflow: hidden;
+  border-radius: 20px;
 }
 .total {
   font-size: 1.5em;
---
+++ src/components/successful-payment.component.vue
@@ -0,0 +1,51 @@
+<script setup>
+import GalleriaComponent from "@/components/services-galleria.component.vue";
+import { ref } from 'vue';
+const visible = ref(false);
+</script>
+
+<template>
+  <div class="save">
+    <pv-button class="btn-pay" label="Pagar"  @click="visible = true " />
+    <pv-dialog v-model:visible="visible" modal header="Pagar Servicio" :style="{ width: '50rem' }"
+               :breakpoints="{ '960px': '75vw', '641px': '100vw' }">
+
+      <div class="contenedor">
+        <h1 style="font-size: 30px;"><b>¡El pago a sido realizado con exito!</b> </h1>
+      </div>
+
+      <pv-card class="tourist-profile" style="width: 45rem">
+
+        <template #content>
+          <div class="content flex gap-5 justify-content-between align-items-center">
+            <galleria-component></galleria-component>
+            <h1 > Alpinismo para principiantes en el Huascarán, se parte de la aventura de vivir  </h1>
+            <ul>
+              <li><b>Por:</b><p>agencia TravelNew</p></li>
+              <li><b>Fecha de Salida: </b><p>12/05/2023</p></li>
+              <li><b>Total de Importe: </b><p>$500.00</p></li>
+              <div class="flex-auto">
+                <b>Total de personas: </b> <pv-inputNumber class="input-number" v-model="value2" inputId="minmax-buttons" mode="decimal" showButtons :min="0" :max="100" />
+              </div>
+            </ul>
+          </div>
+        </template>
+      </pv-card>
+
+      <pv-button class="btn-pay" label="Aceptar" @click="visible = false" />
+    </pv-dialog>
+  </div>
+</template>
+
+<style scoped>
+.btn-pay {
+  width: 100%;
+  margin: 1em 0;
+  background-color: var(--primary-color);
+  color: white;
+  font-size: 0.8em;
+}
+.contenedor {
+  text-align: center;
+}
+</style>
