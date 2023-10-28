bf3fffa98ef852e1fb394c05f26fda2d42c7e67f
OnofreR <carlosonofreruiz16@gmail.com>
2023-10-28 11:27:22 -0500

    update favorite service

--- src/components/pay-service.component.vue
+++ src/components/pay-service.component.vue
@@ -52,8 +52,16 @@ const visible = ref(false);
             </span>
       </div>
 
-      <div class="card flex justify-content-center">
-        <pv-inputMask id="basic" v-model="value" placeholder="99/99/9999" slotChar="mm/dd/yyyy" />
+      <div class="card flex flex-wrap justify-content-left gap-3">
+        <span class="p-input-icon-left">
+            <pv-inputText class= "pay" v-model="value4" placeholder="MM/AA" />
+            </span>
+      </div>
+
+      <div class="card flex flex-wrap justify-content-left gap-3">
+        <span class="p-input-icon-left">
+            <pv-inputText class= "pay" v-model="value4" placeholder="CVC/CVV" />
+            </span>
       </div>
 
       <successful-payment-component></successful-payment-component>
